474c759b25efb7269e5ef055a5fada74bd2dacd2
ThXou <luis14al@gmail.com>
2013-11-15 17:52:33 +0100

    Created library classes

---
+++ LCAnimatedMenu/LCAnimatedMenu/LCAnimatedMenu.h
@@ -0,0 +1,13 @@
+//
+//  LCAnimatedMenu.h
+//  LCAnimatedMenu
+//
+//  Created by ThXou on 14/11/13.
+//  Copyright (c) 2013 ThXou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LCAnimatedMenu : UIView
+
+@end
---
+++ LCAnimatedMenu/LCAnimatedMenu/LCMenuItem.m
@@ -0,0 +1,31 @@
+//
+//  LCMenuItem.m
+//  LCAnimatedMenu
+//
+//  Created by ThXou on 14/11/13.
+//  Copyright (c) 2013 ThXou. All rights reserved.
+//
+
+#import "LCMenuItem.h"
+
+@implementation LCMenuItem
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
